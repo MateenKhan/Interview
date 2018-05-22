44d4a9b6a45a38fb88ebc0edb59a19c1a301a184
MateenKhan <mateen.khan@qount.io>
2018-05-22 09:29:03 +0530

    more design patterns and algorithms

--- src/main/java/sorting/SelectionSortExample.java
+++ src/main/java/sorting/SelectionSortExample.java
@@ -7,12 +7,31 @@ public class SelectionSortExample {
 			int index = i;
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[j] < arr[index]) {
-					index = j;// searching for lowest index
+					index = j;// searching for lowest number index
 				}
 			}
+			//moving smaller number index to the left part of array
+			//and left part number index to the other side
 			int smallerNumber = arr[index];
 			arr[index] = arr[i];
 			arr[i] = smallerNumber;
+		}
+	}
+
+	private static void asdf(int[] input) {
+		if (input.length == 0) {
+			return;
+		}
+		for (int i = 0; i < input.length - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < input.length; j++) {
+				if(input[j]<input[index]) {
+					index=j;
+				}
+			}
+			int temp = input[index];
+			input[index]=input[i];
+			input[i]=temp;
 		}
 	}
 
@@ -24,7 +43,7 @@ public class SelectionSortExample {
 		}
 		System.out.println();
 
-		selectionSort(arr1);// sorting array using selection sort
+		asdf(arr1);// sorting array using selection sort
 
 		System.out.println("After Selection Sort");
 		for (int i : arr1) {
